c8fa42229f1eb5f6e56c17a5f06e0e90e419306e
burtonwilliamt <burtonwilliamt@gmail.com>
2016-11-02 17:32:05 -0400

    increased lanes, added different types of cars

--- demos/TraCI-Platooning-Demo/demo.py
+++ demos/TraCI-Platooning-Demo/demo.py
@@ -24,34 +24,49 @@ while step < 1000:
     idList = traci.vehicle.getIDList()
     # print "ID List: ", idList
     for veh in idList:
-        traci.vehicle.setLaneChangeMode(veh, 0)
+
         # if the next ID is the same as us, then we can platoon
         # nextType = traci.vehicle.getTypeID(//nextID)
         # if we are doing CarLogic, and the nextID is anythng but human, we can platoon
 
+        laneIdStr, idInLaneStr= veh.split("_",1)
+        laneId = int(laneIdStr)
+        idInLane = -1
+        if(idInLaneStr != "roadBlock"):
+            idInLane = int(idInLaneStr)
+        # print("ID: " + laneIdStr)
+        typeID = traci.vehicle.getTypeID(veh)
+
+        # carInFront = "none"
+        # if(idInLane > 1):
+        #     carInFront = laneId+"_"+idInLane-1
+
+        pos = traci.vehicle.getPosition(veh)
+        ang = traci.vehicle.getAngle(veh)
+        speed = traci.vehicle.getSpeed(veh)
+        edge = traci.vehicle.getRoadID(veh)
+        stopState = traci.vehicle.getStopState(veh)
+        isStoped = (stopState % 2 == 1)
+
+        # print "Step: %04d | Position: (%03.3f, %03.3f) | Speed: %02.2f" % (step, pos[0], pos[1], speed)
+
+        traci.vehicle.setLaneChangeMode(veh, 0)
+        # if(laneId == 2):
+
+
+        if(veh.endswith("roadBlock")):
+            if(step == 20):
+                # print "Slowing down"
+                # stop_pos = (pos[0]+10, pos[1])
+                trynastop = True
+                traci.vehicle.setStop(veh, edge, pos[0]+50.0, laneId)
+
+            if isStoped and step == 200:
+                traci.vehicle.resume(veh)
+
+
     traci.simulationStep(.1)
-    pos = traci.vehicle.getPosition("veh0")
-    ang = traci.vehicle.getAngle("veh0")
-    speed = traci.vehicle.getSpeed("veh0")
-    edge = traci.vehicle.getRoadID("veh0")
-    stopState = traci.vehicle.getStopState("veh0")
-    #print "Stop state: ", stopState
-    isStoped = (stopState % 2 == 1)
-    print "Step: %04d | Position: (%03.3f, %03.3f) | Speed: %02.2f" % (step, pos[0], pos[1], speed)
-    if speed >= 2 and not trynastop:
-        print "Slowing down"
-        # stop_pos = (pos[0]+10, pos[1])
-        trynastop = True
-        traci.vehicle.setStop("veh0", edge, pos[0]+20.0)
-    if(isStoped):
-        print "I am stopped"
 
-
-    if(speed < .01):
-        print "I no speed"
-
-    if isStoped and step == 150:
-        traci.vehicle.resume("veh0")
 
     step += 1
 
